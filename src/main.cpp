3713d624f23b67b5848f8311d700932967f00612
Caminin <charlot.rodolphe@gmail.com>
2016-03-09 00:31:31 +0100

    add main

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,13 @@ int main(int argc, char **argv)
 {
     Table mytable(0,0);
     vector<Piece*> mypile;
-#ifdef DEBUG
-
-    string file_name=argc+argv[0];
-    file_name="./txt/pieces_05x05.txt";
+    
+    if(argc>2)
     {
-#else
-    if(argc>1)
-    {
-        string file_name=argv[1];
-#endif
+        string s1=string(argv[1]);
+        string s2=string(argv[2]);
+        string file_name="./txt/pieces_0"+s1+"x0"+s2+".txt";
+        
         parseFile(&mytable,mypile,file_name);
     }
     cout << mypile.size() << endl;
